c39b0b9cb1211cec683676d885bfea9d4a109530
Rutenis Raila <rutis98@gmail.com>
2019-02-17 18:45:36 +0000

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || '4b2ed6449f9aebd6a4eb0efcf50d4d4589716ada7f1a6f8223f7d040a2f888a9',
+    SECRET: process.env.SECRET || '50badf78d8e617ecd2736a03a76a541e13d4cb66fc8b74ce11663d665d9338d8',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
